7468fa4401a439f30f7b51d3a0b5351cad48d947
Niko Salonen <nikotsalonen@gmail.com>
2021-08-21 15:45:22 +0300

    add header

---
+++ components/TheHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="center examplex">
+    <vs-navbar
+      v-model="active"
+      color="#1e2023"
+      text-white
+      square
+      center-collapsed
+      fixed="true"
+      shadow="true"
+    >
+      <vs-navbar-item id="etusivu" :active="active == 'etusivu'">
+        Etusivu
+      </vs-navbar-item>
+      <vs-navbar-item id="settings" :active="active == 'settings'">
+        Asetukset
+      </vs-navbar-item>
+    </vs-navbar>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    active: 'etusivu',
+  }),
+}
+</script>
