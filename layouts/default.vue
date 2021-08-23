09cdcc57e758e7076e42a0a1e357b3b97b731e45
Niko Salonen <nikotsalonen@gmail.com>
2021-08-23 17:38:56 +0300

    flex-grow nux

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,22 @@
 <template>
   <div
     id="app"
-    class="bg-gray-900 h-screen flex flex-col bg-gray-1000 font-body"
+    class="
+      bg-gray-900
+      h-screen
+      flex flex-col
+      bg-gray-1000
+      font-body
+      justify-between
+    "
   >
     <Header />
-    <div class="relative py-3 sm:max-w-xl sm:mx-auto">
+    <div class="flex-grow">
       <Nuxt />
     </div>
-    <Footer />
+    <div class="">
+      <Footer />
+    </div>
   </div>
 </template>
 
