7287c741d0c2671811fdfcc7416f66c81c346f97
Niko Salonen <nikotsalonen@gmail.com>
2021-08-23 15:11:14 +0300

    remove classes

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="app">
-    <h1 class="title">
+  <div class="">
+    <h1 class="">
       Liiga tänään {{ new Date(today).toLocaleDateString('fi-FI') }}
     </h1>
     <div v-for="game in games" :key="game.id">
@@ -24,76 +24,4 @@ export default {
 }
 </script>
 
-<style>
-.app {
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #fff;
-}
-ul li {
-  list-style: none;
-}
-a {
-  color: #fff;
-  text-decoration: none;
-}
-
-a:hover {
-  color: #aaa;
-  text-decoration: underline;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 55px;
-  color: #fff;
-  letter-spacing: 1px;
-  text-transform: capitalize;
-  margin: 25px 0;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 1.1rem;
-  color: #526488;
-  word-spacing: 2px;
-  padding-bottom: 15px;
-  max-width: 600px;
-}
-
-.subtitle a {
-  font-weight: 500;
-  color: inherit;
-}
-
-.links {
-  padding-top: 15px;
-  margin-bottom: 20px;
-}
-
-.content-logos {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  min-width: 500px;
-}
-
-.plus {
-  font-size: 2.5rem;
-  margin: 15px;
-  color: #35495e;
-}
-
-.h3 {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-weight: 400;
-  margin: 10px;
-}
-</style>
+<style></style>
