d3ae10792fb27b83ef29f5fd2136c8322cf992b6
Niko Salonen <nikotsalonen@gmail.com>
2021-08-23 18:20:32 +0300

    footer link fix

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="text-white">
-    <a href="https://github.com/nikosalonen">Fork this page on GitHub</a>
+    <a href="https://github.com/nikosalonen/liiga-lite/"
+      >Fork this page on GitHub</a
+    >
   </div>
 </template>
