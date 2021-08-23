1c16d9fcf79b4e49a633fb4caaa17109a90761d6
Niko Salonen <nikotsalonen@gmail.com>
2021-08-23 15:37:35 +0300

    add body font

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="min-h-screen flex flex-col justify-center bg-gray-1000">
+  <div
+    id="app"
+    class="
+      bg-gray-900
+      min-h-screen
+      flex flex-col
+      justify-center
+      bg-gray-1000
+      font-body
+    "
+  >
     <Header />
     <div class="relative py-3 sm:max-w-xl sm:mx-auto">
       <Nuxt />
@@ -8,24 +18,4 @@
   </div>
 </template>
 
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-  background-color: #18191c;
-}
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-</style>
+<style></style>
