e12a1387e51bc71edd9566b1468d67d6f2892aa5
Niko Salonen <nikotsalonen@gmail.com>
2021-08-27 10:39:15 +0300

    add nuxt-image

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,6 +35,7 @@ export default {
     '@nuxtjs/pwa',
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
+    '@nuxt/image',
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
