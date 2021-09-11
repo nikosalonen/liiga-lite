402998b06b200eb72a5727bd1e21d148522371f0
Niko Salonen <nikotsalonen@gmail.com>
2021-09-11 19:07:45 +0300

    show proper score for ended game

--- components/Game.vue
+++ components/Game.vue
@@ -42,7 +42,7 @@
             }.png`)
           "
         />
-        <div v-if="!started || ended" class="score text-gray-700 text-xl px-4">
+        <div v-if="!started" class="score text-gray-700 text-xl px-4">
           0 - 0
         </div>
         <div v-else class="score text-white text-xl px-4">
