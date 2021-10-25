c3ebc64c703a12de6dad41fcc2d87d767638e06d
Niko Salonen <nikotsalonen@gmail.com>
2021-10-25 09:11:10 +0300

    use eslint-parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,9 @@ module.exports = {
     node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+    ecmaVersion: 8,
   },
   extends: [
     '@nuxtjs',
